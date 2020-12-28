837fcba1cd49ccd0cc806e7479ab042ae4afdafe
Matheus Afonso <matheusafonsouza@gmail.com>
2020-12-28 01:40:11 -0300

    feat: created page and overlay components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Page from './components/Page';
 import { GlobalStyles } from './styles/global';
 
 const App: React.FC = () => {
   return (
     <>
-      <h1>tesla</h1>
+      <Page />
       <GlobalStyles />
     </>
   );
